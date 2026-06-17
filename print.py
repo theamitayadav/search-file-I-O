bdbbd27e27a37032a52af08d8be4911ad7fd660b
AMITA YADAV <yadavamitaaim@gmail.com>
2026-06-17 08:42:12 +0530

    Add files via upload

---
+++ print.py
@@ -0,0 +1,109 @@
+#with open("demo.txt","w") as f:
+    #f.write("Hi Everyone\n we are learning file input\n")
+    #f.write("using python\n I like programming in python")
+
+#with open("demo.txt","r") as f:
+   # data = f.read()
+    #new_data = data.replace("python","fast api")
+#print(new_data)
+
+#from a file containing no. separated by , print the count of even no.
+
+#with open("demo.txt","w") as f:
+    #f.write("1,34,54,87,53,98,65,66")
+
+
+#with open("demo.txt","r") as f: 
+#data = f.read()  
+
+#nums = data.split(",")
+#count = 0
+#for i  in nums:
+    #if(int(i)%2 == 0):{}
+    #count += 1
+
+#print(count)
+
+
+#searching word "learning" exists in the file or not.
+
+def check_for _word():
+
+word = "learing"
+
+with open("sample.txt","r") as f:
+    data = f.read()
+if (data.find (word)!= -1):
+     
+     print (" learning Word exists in the file")
+else:
+    print("learning Word not exists in the file")
+    
+        
+
+   
+
+
+    
+    
+    
+
+
+
+
+
+
+
+
+    
+    
+
+
+
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
